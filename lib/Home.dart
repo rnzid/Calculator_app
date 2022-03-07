56fbe6fcf2aa571019a28a63a5effb9a109e98b9
rnzid <ranjitdangol13@gmail.com>
2022-03-07 22:42:50 +0545

    calculator flex overflow problem solved

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,48 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+  final TextEditingController t1 = TextEditingController(text: "0");
+  final TextEditingController t2 = TextEditingController(text: "0");
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 + num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 - num2;
+    });
+  }
+
+  void doMul() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 * num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+      sum = num1 ~/ num2;
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      t1.text = "0";
+      t2.text = "0";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,24 +60,43 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Container(
         padding: const EdgeInsets.all(40.0),
-        child: Column(
+        child: ListView(
           children: <Widget>[
-            const TextField(
+            Text("Output: $sum",
+                style: const TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple)),
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: "Enter first number"),
+              decoration: const InputDecoration(hintText: "Enter first number"),
+              controller: t1,
             ),
-            const TextField(
+            TextField(
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: "Enter first number"),
+              decoration: const InputDecoration(hintText: "Enter first number"),
+              controller: t2,
             ),
             const Padding(padding: EdgeInsets.only(top: 20.0)),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(onPressed: doAddition, child: const Text("+")),
+                ElevatedButton(onPressed: doSub, child: const Text("-")),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.only(top: 20.0)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(onPressed: doMul, child: const Text("*")),
+                ElevatedButton(onPressed: doDiv, child: const Text("/"))
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ElevatedButton(onPressed: () {}, child: const Text("+")),
-                ElevatedButton(onPressed: () {}, child: const Text("-")),
-                ElevatedButton(onPressed: () {}, child: const Text("*")),
-                ElevatedButton(onPressed: () {}, child: const Text("/"))
+                ElevatedButton(onPressed: doClear, child: const Text("Clear")),
               ],
             )
           ],
